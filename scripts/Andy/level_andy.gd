70e97682363e1cd5f0995dd3fdb43570cd7a82a0
Amodh Dhakal <adhakal2@ncsu.edu>
2025-02-15 17:38:14 -0500

    Got a timer setup

---
+++ scripts/Andy/level_andy.gd
@@ -0,0 +1,40 @@
+extends Node2D
+
+var timeLabel = Label
+var time = Timer
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	timeLabel = $Label
+	time = $Timer
+	
+	time.start()
+
+# Run when the timer times out, 90 seconds
+func _on_timer_timeout():
+	# TODO: Game over if without finding all of the objects
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	updateTimerLabel()
+	pass
+
+# Update the timer label to show the time
+func updateTimerLabel():
+	var secondsRemaining = ceil(time.time_left)
+	
+	if ( secondsRemaining <= 9):
+		timeLabel.text = "0:0" + str(secondsRemaining)
+		return
+	
+	if (secondsRemaining <= 59):
+		timeLabel.text = "0:" + str(secondsRemaining)
+		return
+		
+	secondsRemaining -= 60
+	if ( secondsRemaining <= 9 ):
+		timeLabel.text = "1:0" + str(secondsRemaining)
+		return
+		
+	timeLabel.text = "1:" + str(secondsRemaining)
