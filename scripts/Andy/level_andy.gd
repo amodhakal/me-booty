1eb291b3eb11f2c748d3e3b49b2e8f03667517f9
Amodh Dhakal <adhakal2@ncsu.edu>
2025-02-18 15:14:04 -0500

    Fix Timer

--- scripts/Andy/level_andy.gd
+++ scripts/Andy/level_andy.gd
@@ -5,7 +5,7 @@ var time = Timer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	timeLabel = $Label
+	timeLabel = $TimerLabel
 	time = $Timer
 	
 	time.start()
