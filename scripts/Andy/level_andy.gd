97185bc0781010d962d4e5381b5d265a87a2d580
Amodh Dhakal <adhakal2@ncsu.edu>
2025-02-19 18:59:57 -0500

    Got gameloop basics figured out

--- scripts/Andy/level_andy.gd
+++ scripts/Andy/level_andy.gd
@@ -1,3 +1,6 @@
+# TODO: Fix timer not updating label properly
+# TODO: Prevent sprites from being on top of each other
+
 extends Node2D
 
 @onready var time = $Timer
@@ -5,6 +8,7 @@ extends Node2D
 @onready var targetDisplay = $TargetDisplay
 @onready var gameObjects = $GameObjects
 @onready var gameOverLabel = $GameOverLabel
+@onready var gameWonLabel = $GameWonLabel
 
 # A list of all the hidden objects' textures
 var textures = [
@@ -20,11 +24,15 @@ var textures = [
 # The objects that are currently in-game
 var objectsInGame = []
 
+# Current target index
+var targetIndex = 0
+
 # Maximum sprite size
 const MAX_IMAGE_SIZE = Vector2(50, 50)
 
 func _ready() -> void:
 	gameOverLabel.hide()
+	gameWonLabel.hide()
 	addInitialAssets()
 	time.start()
 
@@ -58,23 +66,30 @@ func addInitialAssets():
 		area.add_child(collision)
 		area.add_child(object)
 		gameObjects.add_child(area)
-		objectsInGame.append(area)
+		objectsInGame.append(object)
 
 		# Handle clicking
 		area.input_event.connect(_on_object_clicked.bind(area, object))
+	
+	updateTargetDisplay()
 
 # Handles clicking on objects
 func _on_object_clicked(viewport, event, shape_idx, area, object):
 	if event is InputEventMouseButton and event.pressed:
-		print("Clicked on:", object.texture.resource_path)
-		# TODO: Handle proper and improper item in target picked
+		# Player loses if click the wrong item
+		var correctObject = objectsInGame[targetIndex]
+		if correctObject != object:
+			handleLoss()
+			return
+
+		# Choose another object
 		area.queue_free()
-		objectsInGame.erase(area)
+		objectsInGame.erase(object)
+		updateTargetDisplay()
 
 # Update the timer label to show the remaining time
 func updateTimerLabel():
 	var secondsRemaining = ceil(time.time_left)
-	
 	if secondsRemaining <= 9:
 		timeLabel.text = "0:0" + str(secondsRemaining)
 	elif secondsRemaining <= 59:
@@ -82,3 +97,21 @@ func updateTimerLabel():
 	else:
 		secondsRemaining -= 60
 		timeLabel.text = "1:" + ("0" + str(secondsRemaining) if secondsRemaining <= 9 else str(secondsRemaining))
+
+func updateTargetDisplay():
+	if objectsInGame.size() > 0:
+		targetDisplay.texture = objectsInGame[targetIndex].texture
+	else:
+		handleWin()
+		
+# Handle losing the game
+func handleLoss():
+	gameOverLabel.show()
+	time.stop()
+
+	
+# Handle winning the game
+func handleWin():
+	# TODO: Show to the next level or something
+	gameWonLabel.show()
+	time.stop()
