bf97f826e31452a1f008fdf5558e7d331c408fb9
Amodh Dhakal <adhakal2@ncsu.edu>
2025-02-19 17:55:33 -0500

    Current changes

--- scripts/Andy/level_andy.gd
+++ scripts/Andy/level_andy.gd
@@ -1,13 +1,29 @@
 extends Node2D
 
-var timeLabel = Label
-var time = Timer
+@onready var time = $Timer
+@onready var timeLabel = $TimerLabel
+@onready var targetDisplay = $TargetDisplay
+@onready var gameObjects = $GameObjects
+@onready var gameOverLabel = $GameOverLabel
+
+# A list of all the hidden objects
+var allAvailableObjects = [
+	preload("res://images/Andy/lantern.png"),
+	preload("res://images/Andy/map.png"),
+	preload("res://images/Andy/scroll.png"),
+	preload("res://images/Andy/ship.png"),
+	preload("res://images/Andy/skull.png"),
+	preload("res://images/Andy/sword.png"),
+	preload("res://images/Andy/treasure.png"),
+]
+
+# The object that needs to be found
+var currentTargetIndex = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	timeLabel = $TimerLabel
-	time = $Timer
-	
+	gameOverLabel.hide()
+	addInitialAssets()
 	time.start()
 
 # Run when the timer times out, 90 seconds
@@ -18,6 +34,20 @@ func _on_timer_timeout():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	updateTimerLabel()
+	pass
+	
+# Initially add assets
+func addInitialAssets():
+	for texture in allAvailableObjects:
+		# Create a new object
+		var object = Sprite2D.new()
+		object.texture = texture
+		
+#		# Select sprite position
+		var viewport = get_viewport_rect().size
+		object.position = Vector2(randf_range(50, viewport.x - 50), randf_range(50, viewport.y - 50))
+		
+		
 	pass
 
 # Update the timer label to show the time
