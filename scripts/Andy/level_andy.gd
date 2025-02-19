c04e6b29e173e0ea17d6bbc391d6b3b24dbd621a
Amodh Dhakal <adhakal2@ncsu.edu>
2025-02-19 18:11:29 -0500

    Got items to show up and disappear

--- scripts/Andy/level_andy.gd
+++ scripts/Andy/level_andy.gd
@@ -6,8 +6,8 @@ extends Node2D
 @onready var gameObjects = $GameObjects
 @onready var gameOverLabel = $GameOverLabel
 
-# A list of all the hidden objects
-var allAvailableObjects = [
+# A list of all the hidden objects' textures
+var textures = [
 	preload("res://images/Andy/lantern.png"),
 	preload("res://images/Andy/map.png"),
 	preload("res://images/Andy/scroll.png"),
@@ -17,54 +17,68 @@ var allAvailableObjects = [
 	preload("res://images/Andy/treasure.png"),
 ]
 
-# The object that needs to be found
-var currentTargetIndex = 0
+# The objects that are currently in-game
+var objectsInGame = []
 
-# Called when the node enters the scene tree for the first time.
+# Maximum sprite size
+const MAX_IMAGE_SIZE = Vector2(50, 50)
+
 func _ready() -> void:
 	gameOverLabel.hide()
 	addInitialAssets()
 	time.start()
 
-# Run when the timer times out, 90 seconds
-func _on_timer_timeout():
-	# TODO: Game over if without finding all of the objects
-	pass
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	updateTimerLabel()
-	pass
-	
 # Initially add assets
 func addInitialAssets():
-	for texture in allAvailableObjects:
-		# Create a new object
+	for texture in textures:
+		# Create an Area2D for detecting clicks
+		var area = Area2D.new()
 		var object = Sprite2D.new()
+		var collision = CollisionShape2D.new()
+		var shape = RectangleShape2D.new()
 		object.texture = texture
 		
-#		# Select sprite position
+		# Resize sprite while maintaining aspect ratio
+		var texture_size = object.texture.get_size()
+		var scale_factor = min(MAX_IMAGE_SIZE.x / texture_size.x, MAX_IMAGE_SIZE.y / texture_size.y)
+		object.scale = Vector2(scale_factor, scale_factor)
+
+		# Set sprite position randomly within the viewport
 		var viewport = get_viewport_rect().size
 		object.position = Vector2(randf_range(50, viewport.x - 50), randf_range(50, viewport.y - 50))
 		
+		# Match collision shape size to the resized sprite
+		shape.size = object.texture.get_size() * scale_factor
+		collision.shape = shape
 		
-	pass
+		# Ensure collision shape is properly positioned
+		collision.position = object.position
+		
+		# Organize node hierarchy
+		area.add_child(collision)
+		area.add_child(object)
+		gameObjects.add_child(area)
+		objectsInGame.append(area)
 
-# Update the timer label to show the time
+		# Handle clicking
+		area.input_event.connect(_on_object_clicked.bind(area, object))
+
+# Handles clicking on objects
+func _on_object_clicked(viewport, event, shape_idx, area, object):
+	if event is InputEventMouseButton and event.pressed:
+		print("Clicked on:", object.texture.resource_path)
+		# TODO: Handle proper and improper item in target picked
+		area.queue_free()
+		objectsInGame.erase(area)
+
+# Update the timer label to show the remaining time
 func updateTimerLabel():
 	var secondsRemaining = ceil(time.time_left)
 	
-	if ( secondsRemaining <= 9):
+	if secondsRemaining <= 9:
 		timeLabel.text = "0:0" + str(secondsRemaining)
-		return
-	
-	if (secondsRemaining <= 59):
+	elif secondsRemaining <= 59:
 		timeLabel.text = "0:" + str(secondsRemaining)
-		return
-		
-	secondsRemaining -= 60
-	if ( secondsRemaining <= 9 ):
-		timeLabel.text = "1:0" + str(secondsRemaining)
-		return
-		
-	timeLabel.text = "1:" + str(secondsRemaining)
+	else:
+		secondsRemaining -= 60
+		timeLabel.text = "1:" + ("0" + str(secondsRemaining) if secondsRemaining <= 9 else str(secondsRemaining))
