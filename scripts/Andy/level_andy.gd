3b3f06e35ab4d4b5313aff60299c4b0c9a12628d
Amodh Dhakal <adhakal2@ncsu.edu>
2025-02-20 00:12:52 -0500

    Setup HOG

--- scripts/Andy/level_andy.gd
+++ scripts/Andy/level_andy.gd
@@ -1,12 +1,9 @@
-# TODO: Fix timer not updating label properly
-# TODO: Prevent sprites from being on top of each other, and the labels
-
 extends Node2D
 
 @onready var time = $Timer
 @onready var timeLabel = $TimerLabel
 @onready var targetDisplay = $TargetDisplay
-@onready var gameObjects = $GameObjects
+@onready var targetFrame = $Frame
 @onready var gameOverLabel = $GameOverLabel
 @onready var gameWonLabel = $GameWonLabel
 
@@ -28,56 +25,78 @@ var objectsInGame = []
 var targetIndex = 0
 
 # Maximum sprite size
-const MAX_IMAGE_SIZE = Vector2(50, 50)
+const MAX_IMAGE_SIZE = Vector2(60, 60)
 
 func _ready() -> void:
 	gameOverLabel.hide()
 	gameWonLabel.hide()
 	addInitialAssets()
 	time.start()
-	
+
 func _process(delta: float) -> void:
 	updateTimerLabel()
-	
+
 # Run when the timer times out, 90 seconds
 func _on_timer_timeout():
 	handleLoss()
 
 # Initially add assets
 func addInitialAssets():
+	var timeLabelRect = Rect2(timeLabel.get_global_transform().origin, timeLabel.size)
+	var targetDisplayRect = Rect2(targetDisplay.get_global_transform().origin, targetDisplay.size)
+
+	var targetFrameTexture = targetFrame.texture
+	var targetFrameSize = Vector2.ZERO
+	if targetFrameTexture: # Check if texture is loaded
+		targetFrameSize = targetFrameTexture.get_size() * targetFrame.scale
+	var targetFrameRect = Rect2(targetFrame.get_global_transform().origin - targetFrameSize / 2, targetFrameSize)
+
+
+	var ui_rects_to_avoid = [timeLabelRect, targetDisplayRect, targetFrameRect]
+
 	for texture in textures:
-		# Create an Area2D for detecting clicks
 		var area = Area2D.new()
 		var object = Sprite2D.new()
 		var collision = CollisionShape2D.new()
 		var shape = RectangleShape2D.new()
+
 		object.texture = texture
-		
-		# Resize sprite while maintaining aspect ratio
+		object.z_index = RenderingServer.CANVAS_ITEM_Z_MAX
+		area.z_index = RenderingServer.CANVAS_ITEM_Z_MAX
+
 		var texture_size = object.texture.get_size()
 		var scale_factor = min(MAX_IMAGE_SIZE.x / texture_size.x, MAX_IMAGE_SIZE.y / texture_size.y)
 		object.scale = Vector2(scale_factor, scale_factor)
-
-		# Set sprite position randomly within the viewport
-		var viewport = get_viewport_rect().size
-		object.position = Vector2(randf_range(50, viewport.x - 50), randf_range(50, viewport.y - 50))
-		
-		# Match collision shape size to the resized sprite
 		shape.size = object.texture.get_size() * scale_factor
 		collision.shape = shape
-		
-		# Ensure collision shape is properly positioned
-		collision.position = object.position
-		
-		# Organize node hierarchy
+
+		var viewport = get_viewport_rect().size
+		var sprite_size = shape.size
+		var margin = 20
+		var valid_position = false
+		while not valid_position:
+			var random_position = Vector2(randf_range(sprite_size.x / 2 + margin, viewport.x - sprite_size.x / 2 - margin), randf_range(sprite_size.y / 2 + margin, viewport.y - sprite_size.y / 2 - margin))
+			area.position = random_position
+			var area_rect = Rect2(area.global_position - sprite_size / 2, sprite_size)
+
+			var overlaps_ui = false
+			for ui_rect in ui_rects_to_avoid:
+				if area_rect.intersects(ui_rect):
+					overlaps_ui = true
+					break
+
+			var overlapping_areas = area.get_overlapping_areas()
+			if overlapping_areas.size() == 0 and not overlaps_ui:
+				valid_position = true
+
+		collision.position = Vector2.ZERO
 		area.add_child(collision)
 		area.add_child(object)
-		gameObjects.add_child(area)
+		add_child(area)
 		objectsInGame.append(object)
 
-		# Handle clicking
 		area.input_event.connect(_on_object_clicked.bind(area, object))
-	
+
 	updateTargetDisplay()
 
 # Handles clicking on objects
@@ -97,39 +116,49 @@ func _on_object_clicked(viewport, event, shape_idx, area, object):
 # Update the timer label to show the remaining time
 func updateTimerLabel():
 	var secondsRemaining = ceil(time.time_left)
-	
+
 	if ( secondsRemaining <= 9):
 		timeLabel.text = "0:0" + str(secondsRemaining)
 		return
-	
+
 	if (secondsRemaining <= 59):
 		timeLabel.text = "0:" + str(secondsRemaining)
 		return
-		
+
 	secondsRemaining -= 60
 	if ( secondsRemaining <= 9 ):
 		timeLabel.text = "1:0" + str(secondsRemaining)
 		return
-		
+
 	timeLabel.text = "1:" + str(secondsRemaining)
 
+# Update the target display to show the correct object
 func updateTargetDisplay():
 	if objectsInGame.size() > 0:
 		targetDisplay.texture = objectsInGame[targetIndex].texture
 	else:
 		handleWin()
-		
-# Handle losing the game
-func handleLoss():
-	gameOverLabel.show()
-	timeLabel.hide()
-	time.stop()
 
-	
-# Handle winning the game
-func handleWin():
-	# TODO: Show to the next level or something
-	gameWonLabel.show()
+# Handle end of the game
+func handleEnd():
+	for object in objectsInGame:
+		object.hide()
+
+	targetFrame.hide()
 	timeLabel.hide()
 	targetDisplay.hide()
 	time.stop()
+
+# Handle losing the game
+func handleLoss():
+	gameOverLabel.show()
+	handleEnd()
+
+	for object in objectsInGame:
+		object.hide()
+
+# Handle winning the game
+func handleWin():
+	# Show to the next level or something
+	gameWonLabel.show()
+	handleEnd()
